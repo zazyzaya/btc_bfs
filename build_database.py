e34b558413f6d84775b4eb6d5ff57706b55b4dac
Isaiah <zazyzaya@gmail.com>
2024-03-24 13:26:57 -0400

    Graph builder works

--- build_database.py
+++ build_database.py
@@ -1,8 +1,16 @@
-from globals import BLOCK_DIR, NEWEST, IGNORE
+from globals import BLOCK_DIR, BLOCK_IDX, IGNORE
+import pickle 
 import time 
 
 from blockchain_parser.blockchain import Blockchain
 from tqdm import tqdm 
+
+def get_block_height():
+    with open(BLOCK_IDX, 'r') as f:
+        return [
+            int(i) for i in 
+            f.read().split('\n')[:-1]
+        ]
 
 def build_output_to_wallet_job(fnum):
     '''
@@ -17,7 +25,8 @@ def build_output_to_wallet_job(fnum):
             -[:OWNED_BY]-> 
         (wallet:WALLET {value: tx.address})
     '''
-    db = dict() 
+    addr_db = dict() 
+    value_db = dict()
     bc = Blockchain(f'{BLOCK_DIR}/blk{str(fnum).zfill(5)}.dat')
 
     for block in bc.get_unordered_blocks():
@@ -29,13 +38,88 @@ def build_output_to_wallet_job(fnum):
                 key = f'{tx.txid}:{i}'
                 val = out.addresses[0].address
 
-                db[key] = val 
+                addr_db[key] = val 
+                value_db[key] = out.value
 
-    return db 
+    return addr_db, value_db
+
+def build_graph(fnum, addr_db, value_db):
+    db = dict() 
+    bc = Blockchain(f'{BLOCK_DIR}/blk{str(fnum).zfill(5)}.dat')
+
+    bh = get_block_height()
+    num_blocks = bh[fnum+1]-bh[fnum]
+
+    edges = []
+    for block in tqdm(bc.get_unordered_blocks(), total=num_blocks):
+        for tx in block.transactions:
+            ins = []
+            outs = []
+            edge_weights = []
+            
+            for out in tx.outputs:
+                if out.type in IGNORE:
+                    continue 
+
+                outs.append(out.addresses[0].address)
+                edge_weights.append(out.value)
+
+
+            in_weights = []
+            uncertain = []
+            for i,input in enumerate(tx.inputs):
+                key = f'{input.transaction_hash}:{input.transaction_index}'
+                maybe_addr = addr_db.get(key, key)
+                
+                # Ignore giving yourself change
+                if maybe_addr in outs: 
+                    idx = outs.index(maybe_addr)
+                    outs.pop(idx)
+                    edge_weights.pop(idx)
+
+                    continue 
+
+                if (maybe_weight := value_db.get(key, 0)) == 0:
+                    uncertain.append(i)
+
+                ins.append(maybe_addr)
+                in_weights.append(maybe_weight)
+
+            tot = sum(edge_weights)
+            
+            # Not sure why this happens..
+            if tot == 0:
+                continue 
+
+            dif = tot - sum(in_weights)
+            
+            # Need to distribute remaining tx weight evenly 
+            # across unknown tx inputs
+            if uncertain:
+                uncertain_weight = dif / len(uncertain)
+                for i in uncertain:
+                    in_weights[i] = uncertain_weight
+
+            # Else dif should == 0
+
+            # Spread weights evenly according to input amount
+            for i in range(len(in_weights)):
+                in_weights[i] /= tot 
+
+            # Add edge from every input to every output 
+            # (Not strictly correct, but no way to really seperate  
+            # them if they're in  the same transaction)
+            for i in range(len(ins)): 
+                for j in range(len(outs)):
+                    edges.append(
+                        (ins[i], outs[j], str(int(in_weights[i] * edge_weights[j])))
+                    )
+
+    return edges
 
 def test_output_mapper():
     st = time.time()
-    db = build_output_to_wallet_job(10)
+    db,_ = build_output_to_wallet_job(10)
     uq = set(list(db.values()))
     print(f'{len(db)-len(uq)}/{len(db)} addresses seen only once')
     print(f"Runtime: {time.time()-st}s")
@@ -47,5 +131,29 @@ def test_output_mapper():
     Runtime: 21.117431640625s
     '''
 
+def test_build_graph(start_db,end_db):
+    '''
+    addrs, vals = dict(), dict()
+    
+    # Build small, in-memory database of first 3 files
+    for i in range(start_db,end_db):
+        a,v = build_output_to_wallet_job(i)
+        addrs = addrs | a 
+        vals = vals | v
+
+    # Save to disk so don't have to run above every time
+    with open('dbs.pkl', 'wb+') as f:
+        pickle.dump((addrs,vals), f)
+    '''
+    with open('dbs.pkl', 'rb') as f:
+        addrs,vals = pickle.load(f)
+        
+    # Try to build a graph of just the last file 
+    g = build_graph(end_db-1, addrs, vals)
+    
+    with open('subgraph.csv', 'w') as f:
+        for row in g:
+            f.write(','.join(row) + '\n')
+
 if __name__ == '__main__':
-    test_output_mapper()
+    test_build_graph(0,3)
--- globals.py
+++ globals.py
@@ -1,3 +1,4 @@
 BLOCK_DIR = '/mnt/raid1_ssd_4tb/datasets/bitcoin/bitcoin-25.0/.bitcoin/blocks'
 IGNORE = ['OP_RETURN', 'p2ms', 'invalid', 'unknown']
 NEWEST = 4185
+BLOCK_IDX = f'{BLOCK_DIR}/blk_index.txt'
